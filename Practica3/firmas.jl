ff6af3e04a4f5685129a7f2d5c75edd5de556326
adrian tinaquero <adrian.tinaquero@udc.es>
2025-10-24 11:40:03 +0200

    fix: addBatch!

--- Practica3/firmas.jl
+++ Practica3/firmas.jl
@@ -606,7 +606,7 @@ end;
 function initializeStreamLearningData(datasetFolder::String, windowSize::Int, batchSize::Int)
     full_dataset = loadStreamLearningDataset(datasetFolder)
     memory = selectInstances(full_dataset, 1:windowSize)
-    rest = selectInstances(full_dataset, (windowSize+1):size(full_dataset[1], 2))
+    rest = selectInstances(full_dataset, (windowSize+1):length(full_dataset[2]))
     batches = divideBatches(rest, batchSize; shuffleRows=false)
     return memory, batches
 end;
@@ -619,12 +619,12 @@ function addBatch!(memory::Batch, newBatch::Batch)
     len_memory = size(memory_inputs, 2)           
 
     # desplaza la memoria, es decir, quita primeras instancias
-    memory_inputs[:, 1:(len_memory-len_new_instances)] .= memory_inputs[:, (len_new_instances+1):len_memory]
-    memory_targets[:, 1:(len_memory-len_new_instances)] .= memory_targets[:, (len_new_instances+1):len_memory]
+    memory_inputs[1:(len_memory-len_new_instances), :] .= memory_inputs[(len_new_instances+1):len_memory, :]
+    memory_targets[1:(len_memory-len_new_instances)] .= memory_targets[(len_new_instances+1):len_memory]
 
     # copia nuevos datos al final
-    memory_inputs[:, (len_memory-len_new_instances+1):len_memory] .= new_inputs
-    memory_targets[:, (len_memory-len_new_instances+1):len_memory] .= new_targets
+    memory_inputs[(len_memory-len_new_instances+1):len_memory, :] .= new_inputs
+    memory_targets[(len_memory-len_new_instances+1):len_memory] .= new_targets
 
     return nothing   # no devuelve nada porque solo modifica la variable memory
 end;
