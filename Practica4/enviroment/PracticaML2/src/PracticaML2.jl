23702175572495346e7d0616753bda6e314a96bb
NoelPanBrea <npanbrea@gmail.com>
2025-11-25 10:46:52 +0100

    fix: Cambié la estructura de carpetas

---
+++ Practica4/enviroment/PracticaML2/src/PracticaML2.jl
@@ -0,0 +1,5 @@
+module PracticaML2
+
+greet() = print("Hello World!")
+
+end # module PracticaML2
--- Practica4/src/data.jl
+++ Practica4/src/data.jl
@@ -1,20 +1,13 @@
 include("firmas.jl")
 
-using CSV
-using DataFrames
-
 
 function CreateDataset(datasetFolder::String)
     files = fileNamesFolder(datasetFolder, "csv")
     datasets_list = loadDataset.(files, dataset_folder)
     merged_data = DataFrame()
     for dataset in datasets_list
-    merged_data = vcat(merged_data, dataset)
+        merged_data = vcat(merged_data, dataset)
     end
 
     return merged_data
 end
-
-dataset_folder = "Practica4/dataset"
-dataset = CreateDataset(dataset_folder)
-CSV.write("Practica4/createddataset/merged.csv", dataset)
