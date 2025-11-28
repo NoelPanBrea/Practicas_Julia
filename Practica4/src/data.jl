4d9ac009be408e6001c6f0cc9d8b54e67499d12a
NoelPanBrea <npanbrea@gmail.com>
2025-11-28 10:36:04 +0100

    feat: Parte 1 y principios parte 2

--- Practica4/src/data.jl
+++ Practica4/src/data.jl
@@ -2,12 +2,47 @@ include("firmas.jl")
 
 
 function CreateDataset(datasetFolder::String)
-    files = fileNamesFolder(datasetFolder, "csv")
-    datasets_list = loadDataset.(files, dataset_folder)
-    merged_data = DataFrame()
+    files = fileNamesFolder(datasetFolder, "csv");
+    datasets_list = loadDataset.(files, dataset_folder);
+    merged_data = DataFrame();
     for dataset in datasets_list
-        merged_data = vcat(merged_data, dataset)
-    end
+        merged_data = vcat(merged_data, dataset);
+    end;
+    return merged_data;
+end;
 
-    return merged_data
+function OneHotEncoding(labels::AbstractArray{<:Any,1})
+    classes = convert(AbstractArray{<:Any, 1}, unique(labels));
+    if length(classes) > 2
+        return  convert(BitArray{2}, hcat([label .== classes for label in labels]...)');
+    else
+        return reshape(convert(AbstractArray{Bool,1}, labels .== classes.first()), :, 1);
+    end;
+end;
+
+function holdOut(N::Int, P::Real)
+    permutation = Random.randperm(N);
+    test_index = permutation[1:(convert(Int64, (round(N*P))))];
+    train_index = permutation[(convert(Int64, (round(N*P))) + 1):end];
+    index = (train_index, test_index);
+    return index;
 end
+
+function crossvalidation(n::Int64, k::Int64)
+    v = 1:k;
+    v_repeated = repeat(v, convert(Int64, ceil(n/k)));
+    v_sliced = v_repeated[1:n];
+    return Random.shuffle!(v_sliced);
+end;
+
+function crossvalidation(individuals::AbstractArray{<:Any,1}, k::Int64)
+    n_ins = size(individuals, 1);
+    n_inv = unique(individuals);
+    indexes = zeros(Int64, n_ins);
+    inv_indx = crossvalidation(length(n_inv), k)
+    for i in 1:k
+        inv_pos = findall(x -> x == i, inv_indx);
+        indexes[findall(x -> any(x .== n_inv[inv_pos]), individuals)] .= i
+    end;
+    return indexes;
+end;
---
+++ Practica4/src/filters.jl
@@ -0,0 +1,10 @@
+function kendall(inputs::DataFrame)
+    CP = 0;
+    for i in eachrow(inputs)
+        for j in eachrow(inputs)
+            if 
+end;
+
+
+function anova()
+end;
