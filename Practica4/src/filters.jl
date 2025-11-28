be2e36fc22b1770214fefe99a771e945d7862d53
NoelPanBrea <npanbrea@gmail.com>
2025-11-28 12:15:03 +0100

    feat: kendall tau

--- Practica4/src/filters.jl
+++ Practica4/src/filters.jl
@@ -1,8 +1,17 @@
-function kendall(inputs::DataFrame)
+function kendall(dataset::Tuple{DataFrame, BitArray})
     CP = 0;
-    for i in eachrow(inputs)
-        for j in eachrow(inputs)
-            if 
+    targets = findall.(eachrow(dataset[2]));
+    inputs = dataset[1];
+    for (i, x) in zip(eachrow(inputs), targets)
+        for (j, y) in zip(eachrow(inputs), targets)
+            t1 = any(i .< j & x < y)
+            t2 = any(i .> j & x > y)
+            if j!=i & (t1 || t2)
+                CP += count(t1) + count(t2)
+            end;
+        end;
+    end;
+    return CP
 end;
 
 
