2ae1e4ddee2173b16b8823057b15b35342011ab4
adrian tinaquero <adrian.tinaquero@udc.es>
2025-12-05 11:07:19 +0100

    fix: anova

--- Practica4/src/filters.jl
+++ Practica4/src/filters.jl
@@ -19,14 +19,19 @@ end;
 function anova(dataset::Tuple{DataFrame, BitArray})
     inputs, labels = dataset; 
     means = mean.(inputs[:, eachcol(inputs)]); 
-    classes = unique(labels, dims = 1); 
+    targets = findall(eachrow[labels]);
+    classes = unique(targets);
     mean_class = [];
-    for class in classes 
+    for class in classes
         index = findall(row -> row == class, eachrow(labels)); 
         push!(mean_class, mean(inputs[index, eachcol(inputs)])); 
     end;
 
-    sst = sum((inputs[:, eachcol(inputs)] .- means) .^ 2); 
-    sse = sum([sum((inputs[labels .== c, :] .- mean_class[c]).^2) for c in classes])
+    sst = sum((inputs[:, eachcol(inputs)] .- means) .^ 2);
+    sse = 0;
+    for c in classes
+        index = findall(row -> row == class, eachrow(labels)); 
+        sse += sum([sum((inputs[index, :] .- mean_class[c]).^2)]);
+    end;
     f = (sst - sse) / (length(classes) - 1) / (sse / (size(inputs, 1) - length(classes)));
 end;
