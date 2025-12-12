e9584d9a2d002fd788b832cb9807da5ae8c3b696
adrian tinaquero <adrian.tinaquero@udc.es>
2025-12-12 11:40:01 +0100

    feat: filtro pearson

--- Practica4/src/filters.jl
+++ Practica4/src/filters.jl
@@ -24,25 +24,40 @@ end;
 function anova(dataset::Tuple{DataFrame, BitArray})
     inputs, labels = dataset; 
     means = mean.(eachcol(inputs));
-    targets = sum.(findall.(eachrow(labels)));
+    targets = [sum(findall(row)) for row in eachrow(labels)]
     classes = unique(targets);
-    mean_class = [];
-    print(classes)
-    # print(targets)
+    mean_class = Dict{Int, Vector{Float64}}();
     for class in classes
-        index = findall(x -> x == class, labels);
-        println(index);
-        push!(mean_class, mean.(eachcol(inputs[index, :]))); 
+        index = findall(x -> x == class, targets);
+        mean_class[class] = mean.(eachcol(inputs[index, :])); 
     end;
-    println(mean_class)
 
     sst = sum(sum([(col .- mn) .^ 2 for (mn, col) in zip(means, eachcol(inputs))]));
-    print(sst)
     sse = 0;
-    for c in classes
-        index = findall(row -> row == c, eachrow(labels));
-        println(size([(col .- mean_class[c]) .^ 2 for col in eachcol(inputs[index, :])]))
-        sse += sum(sum([(col .- mean_class[c]) .^ 2 for col in eachcol(inputs[index, :])]));
+    for class in classes
+        index = findall(x -> x == class, targets)
+        sse += sum(sum([(col .- mean_class[class]) .^ 2 for col in eachcol(inputs[index, :])]));
     end;
+
     return (sst - sse) / (length(classes) - 1) / (sse / (size(inputs, 1) - length(classes)));
 end;
+
+
+
+
+function pearson(dataset::Tuple{DataFrame, BitArray}; threshold=0.1)
+    inputs, labels = dataset
+    targets = [sum(findall(row)) for row in eachrow(labels)]
+    correlations = Dict{String, Float64}()
+    
+    for (col_name, col) in zip(names(inputs), eachcol(inputs))
+        # he creado las variables numerador y denominador para que no quede muy larga la expresión de correlación
+        numerator = (sum((col .- mean(col)) .* (targets .- mean(targets))))
+        denominator = (sqrt(sum((col .- mean(col)).^2)) * sqrt(sum((targets .- mean(targets)).^2)))
+        correlations[col_name] =  numerator / denominator
+    end
+
+    filtered_cols = [col for col in keys(correlations) if abs(correlations[col]) ≥ threshold]
+
+    return inputs[:, filtered_cols], correlations
+end
