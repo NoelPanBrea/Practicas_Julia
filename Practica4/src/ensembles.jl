b226fb90afc39064f02999706d4072a36639d285
NoelPanBrea <npanbrea@gmail.com>
2025-12-19 14:14:45 +0100

    feat: ensembles evaluacion, crossvalidation arreglada, Seleccion de mejores modelos

--- Practica4/src/ensembles.jl
+++ Practica4/src/ensembles.jl
@@ -1,4 +1,4 @@
-function TrainBaseEnsembles(modelType::Symbol, modelHyperparameters::Dict, dataset::Tuple{DataFrame, BitMatrix}, crossValidationIndices::Array{Int64,1}, k::Int)
+function TrainCrossValEnsembles(modelType::Symbol, modelHyperparameters::Dict, dataset::Tuple{DataFrame, BitMatrix}, crossValidationIndices::Array{Int64,1})
     accuracy = Float64[];
     sensitivity = Float64[];
     ppv = Float64[];
@@ -9,11 +9,10 @@ function TrainBaseEnsembles(modelType::Symbol, modelHyperparameters::Dict, datas
         trainIndices = findall(x -> x != fold, crossValidationIndices);
         testIndices = findall(x -> x == fold, crossValidationIndices);
 
-        train_inputs = pca(inputs[trainIndices, :], n_components=k)[1];
-        train_targets = categorical(sum.(findall.(eachrow(targets[trainIndices, :]))));
-        test_inputs = pca(inputs[testIndices, :], n_components=k)[1];
-        test_targets = categorical(sum.(findall.(eachrow(targets[testIndices, :]))));
-
+        train_inputs = pca(inputs[trainIndices, :], 0.95)[1];
+        train_targets = categorical(sum.(findall.(eachrow(targets[trainIndices, :]))), ordered = true, labels=1:6);
+        test_inputs = pca(inputs[testIndices, :], n_components=size(train_inputs, 2))[1];
+        test_targets = categorical(sum.(findall.(eachrow(targets[testIndices, :]))), ordered = true, labels=1:6);
 
         if modelType == :AdaB
             model = AdaBoost(n_iter=modelHyperparameters["n_iter"])
@@ -52,4 +51,66 @@ function TrainBaseEnsembles(modelType::Symbol, modelHyperparameters::Dict, datas
     ppv_stats = (mean(ppv), std(ppv));
     f1_stats = (mean(f1), std(f1));
     return accuracy_stats, sensitivity_stats, ppv_stats, f1_stats;
+end;
+
+function TrainEnsembles(modelType::Symbol, modelHyperparameters::Dict, dataset::Tuple{Tuple{DataFrame, BitMatrix}, Tuple{DataFrame, BitMatrix}})
+    ((train_inputs, train_targets), (test_inputs, test_targets)) = dataset;
+
+    train_inputs = pca(train_inputs, 0.95)[1];
+    test_inputs = pca(test_inputs, n_components=size(train_inputs, 2))[1];
+    train_targets = categorical(sum.(findall.(eachrow(train_targets))));
+    test_targets = categorical(sum.(findall.(eachrow(test_targets))));
+
+    if modelType == :AdaB
+            model = AdaBoost(n_iter=modelHyperparameters["n_iter"])
+            mach = machine(model, train_inputs, train_targets, scitype_check_level=0);
+            MLJ.fit!(mach, verbosity=0);
+            pred = MLJ.predict(mach, test_inputs);
+            predictions = mode.(pred);
+        elseif modelType == :BaggingC
+            model = MLJ.EnsembleModel(model=kNNClassifier(K=5),
+                                    bagging_fraction=1,
+                                    n=modelHyperparameters["n_estimators"])
+            mach = machine(model, train_inputs, train_targets, scitype_check_level=0);
+            MLJ.fit!(mach, verbosity=0);
+            pred = MLJ.predict(mach, test_inputs);
+            predictions = mode.(pred);
+
+        elseif modelType == :EvoT
+            model = EvoTreeClassifier(nrounds=modelHyperparameters["n_estimators"],
+                               eta=0.2);
+
+            mach = machine(model, train_inputs, train_targets);
+            MLJ.fit!(mach, verbosity=0);
+            pred = MLJ.predict(mach, test_inputs);
+            predictions = mode.(pred);
+        elseif modelType == :XGB
+            model = XGBoostClassifier();
+
+            mach = machine(model, train_inputs, train_targets);
+            MLJ.fit!(mach, verbosity=0);
+            pred = MLJ.predict(mach, test_inputs);
+            predictions = mode.(pred);
+        elseif modelType == :CatB
+            model = CatBoostClassifier();
+
+            mach = machine(model, train_inputs, train_targets);
+            MLJ.fit!(mach, verbosity=0);
+            pred = MLJ.predict(mach, test_inputs);
+            predictions = mode.(pred);
+        elseif modelType == :LGBM
+            model = LGBMClassifier();
+
+            mach = machine(model, train_inputs, train_targets);
+            MLJ.fit!(mach, verbosity=0);
+            pred = MLJ.predict(mach, test_inputs);
+            predictions = mode.(pred);
+        end;
+
+        accuracy = MLJ.accuracy(predictions, test_targets)
+        sensitivity = MLJ.sensitivity(predictions, test_targets)
+        ppv = MLJ.ppv(predictions, test_targets)
+        f1score = MLJ.f1score(predictions, test_targets)
+
+    return accuracy, sensitivity, ppv, f1score;
 end; 
